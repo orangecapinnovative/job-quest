8954cf69dcd4b07749023d1e27f9c50b1e003810
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2016-09-27 18:21:59 +0700

    clear everything

--- backend/trip.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let mongoose     = require('mongoose');
-let Schema       = mongoose.Schema;
-let TripSchema = new Schema({name:String,price:Number,description:String})
-
-module.exports = mongoose.model('Trip', TripSchema);
